3851155639c368e54354c8c68b49decedb94364b
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2018-03-06 01:52:54 +0900

    Setup unit/e2e testings

---
+++ test/run-unit.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+require('reify');
+require('buble-register');
+
+var path = require('path')
+  , glob = require('glob')
+  ;
+
+process.argv.slice(2).forEach(function (arg) {
+  glob(arg, function (err, files) {
+    if (err) throw err
+
+    files.forEach(function (file) {
+      require(path.resolve(process.cwd(), file))
+    })
+  })
+});
